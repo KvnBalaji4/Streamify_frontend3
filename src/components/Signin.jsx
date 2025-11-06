044f03191f32b243663dce6ec69cf221b27d0afe
KvnBalaji4 <venkatanagabalaji124@gmail.com>
2025-11-06 10:38:58 +0530

    Update API endpoint for login request

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -17,7 +17,7 @@ const Signin = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:6086/api/login", {
+      const response = await fetch("http://54.163.42.54:6086/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
