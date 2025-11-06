b9c635568273eb75b76ccc069f43bb92c950bfc6
KvnBalaji4 <venkatanagabalaji124@gmail.com>
2025-11-06 10:38:10 +0530

    Update MovieSection.jsx

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -18,7 +18,7 @@ const MovieSection = ({ title, movies }) => {
   const addToWatchlist = (videoId) => {
     const username = localStorage.getItem("username"); // ✅ now using username
     fetch(
-      `http://localhost:6086/api/watchlist/add?username=${username}&videoId=${videoId}`,
+      `http://54.163.42.54:6086/api/watchlist/add?username=${username}&videoId=${videoId}`,
       { method: "POST" }
     )
       .then((res) => res.json())
