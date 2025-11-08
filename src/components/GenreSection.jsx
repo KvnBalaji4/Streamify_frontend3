c8cbb033709624c59cdbe39595118286bfe76271
MURALA SAI GANESH <163727585+MSGanesh23@users.noreply.github.com>
2025-11-08 08:46:11 +0530

    Update API endpoint for genre data retrieval

--- src/components/GenreSection.jsx
+++ src/components/GenreSection.jsx
@@ -6,7 +6,7 @@ const GenreSection = ({ genre }) => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-   axios.get(`http://54.163.42.54:6086/api/genre/${genre}`, {
+   axios.get(`http://98.91.0.193:6086/api/genre/${genre}`, {
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
